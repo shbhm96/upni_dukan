87db6216ac4aa717a7ce36700e4bbf62b4a15a0f
shbhm96 <shbhm96@gmail.com>
2023-05-11 23:36:55 +0530

    Product Page

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,7 +4,7 @@ import Rating from './Rating'
 import { Link } from 'react-router-dom'
 
 const Product = ({product}) => {
-  {console.log({product})}
+  
   return (
      <Card className='my-3 p-3 rounded'>
       <Link to={`/product/${product._id}`}>
